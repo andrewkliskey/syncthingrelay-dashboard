b9afef86499e732fb72521bcdd70cc3827f4f940
andrewkliskey <44645768+andrewkliskey@users.noreply.github.com>
2020-05-17 14:23:32 +0100

    UI Improvments

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function reqListener() {
   // Parse the JSON text to an object so we can get just one property
   var data = JSON.parse(this.responseText);
   // Append that value to the DOM.
-  document.querySelector("#totaldata").innerHTML = (Math.round(data.bytesProxied / 1000000000).toFixed(2));
+  document.querySelector("#totaldata").innerHTML = (Math.round(data.bytesProxied / 1000000000).toFixed(0));
   document.querySelector("#sessions").innerHTML = data.numActiveSessions;
   document.querySelector("#connections").innerHTML = data.numConnections;
   document.querySelector("#uptime").innerHTML = (Math.round(data.uptimeSeconds/3600).toFixed(0));
